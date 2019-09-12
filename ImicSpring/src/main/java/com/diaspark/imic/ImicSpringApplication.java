f0d6008b97dfa2611f580e16ae8a8287043727c7
Himanshu1843-CLAD <himanshu.khatri@diaspark.com>
2019-09-12 19:23:12 +0530

    Java structure added

---
+++ ImicSpring/src/main/java/com/diaspark/imic/ImicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.diaspark.imic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImicSpringApplication.class, args);
+	}
+
+}
