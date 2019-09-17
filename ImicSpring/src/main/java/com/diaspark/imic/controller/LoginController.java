43e119da6acbd6a3d7e1e9f6d08b6bd3a6ff2cb1
shivangirai29diaspark <shivangi.rai@diaspark.com>
2019-09-17 12:08:40 +0530

    controller files added

---
+++ ImicSpring/src/main/java/com/diaspark/imic/controller/LoginController.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.diaspark.imic.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.diaspark.imic.model.LoginUser;
+import com.diaspark.imic.model.User;
+import com.diaspark.imic.service.LoginService;
+import com.diaspark.imic.service.LoginServiceImpl;
+
+/**
+ * @author SHIVANGI RAI
+ *
+ */
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+@RequestMapping("/login")
+public class LoginController {
+	
+	@Autowired
+	LoginService loginService;
+	
+	//@Autowired
+	//LoginServiceImpl loginServiceImpl;
+	
+	@PostMapping("/")
+	public User login(@RequestBody @Valid LoginUser loginUser) {
+		return loginService.login(loginUser);
+	}
+
+}
